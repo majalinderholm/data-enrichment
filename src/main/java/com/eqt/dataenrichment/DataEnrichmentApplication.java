39844406743b9ebaff2572d81e64765e73960fe1
Maja Linderholm <maja.linderholm@gmail.com>
2024-03-24 09:19:12 +0100

    Initialized repo

---
+++ src/main/java/com/eqt/dataenrichment/DataEnrichmentApplication.java
@@ -0,0 +1,13 @@
+package com.eqt.dataenrichment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataEnrichmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataEnrichmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eqt/dataenrichment/DataEnrichmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eqt.dataenrichment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataEnrichmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
